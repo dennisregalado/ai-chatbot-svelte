c455cca02f30e2ea750fdde23650c30e2f121232
Dennis Regalado <35279373+dennisregalado@users.noreply.github.com>
2025-08-20 11:53:36 -0500

    refactor: Update message component styling and structure for improved layout and responsiveness

--- src/lib/components/message.svelte
+++ src/lib/components/message.svelte
@@ -133,7 +133,11 @@
 
 {#snippet thinkingMessage()}
 	{@const role = 'assistant'}
-	<div data-role={role} in:fly={{ opacity: 0, y: 5, delay: 1000 }}>
+	<div
+		class="group/message mx-auto min-h-96 w-full max-w-3xl px-4"
+		data-role={role}
+		in:fly={{ opacity: 0, y: 5, delay: 1000 }}
+	>
 		<div
 			class={cn(
 				'flex w-full gap-4 rounded-xl group-data-[role=user]/message:ml-auto group-data-[role=user]/message:w-fit group-data-[role=user]/message:max-w-2xl group-data-[role=user]/message:px-3 group-data-[role=user]/message:py-2',
@@ -145,7 +149,6 @@
 			<div class="flex size-8 shrink-0 items-center justify-center rounded-full ring-1 ring-border">
 				{@render SparklesIcon(14)}
 			</div>
-
 			<div class="flex w-full flex-col gap-2">
 				<div class="flex flex-col gap-4 text-muted-foreground">Hmm...</div>
 			</div>
